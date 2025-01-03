c6f3b141aa85ab194d2a499d084f3eca4715e5e3
thanhhuynh1903 <thanhgaming1234@gmail.com>
2025-01-03 15:44:42 +0700

    dwa

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import TextView from './components/TextView/TextView';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextView/>
       <StatusBar style="auto" />
     </View>
   );
